a4ad67021aea558ad78d10f32a29129d9386aa6b
Rasta341 <Sir.pokerstar@yandex.ru>
2022-06-22 23:43:07 +0300

    changes in build.gradle

---
+++ src/main/java/hexlet/code/App.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+public class App {
+    public static void main(String[] args) {
+        System.out.println("""
+            Please enter the game number and press Enter.
+            1 - Greet
+            2 - Even
+            3 - Calc""");
+        Cli.selectGame();
+    }
+}
