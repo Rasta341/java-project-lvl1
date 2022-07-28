d3aa9c04984219984e401ae06534bf8b7bc3db30
Rasta341 <Sir.pokerstar@yandex.ru>
2022-07-28 21:29:33 +0300

    make changes in view of progression

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -3,13 +3,14 @@ import hexlet.code.Engine;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+//import org.apache.commons.lang3.ArrayUtils;
 public class Progression {
     public static void progression() {
         Engine.greet();
         System.out.println("What number is missing in the progression?");
         while (Engine.isWin()) {
             int x = Engine.getRandomNum10();
-            int d = Engine.getRandomNum100() + 1;
+            int d = Engine.getRandomNum10() + 1;
             final int arrLength = 10;
             int[] arr = new int[arrLength];
             arr[0] = x;
@@ -26,7 +27,11 @@ public class Progression {
             Engine.setCorrectAnswer(correctAnswer);
             arr2.remove(index);
             arr2.add(index, "..");
-            System.out.println("Question: " + arr2);
+            String str = String.valueOf(arr2);
+            System.out.println("Question: " + str.replace('[', ' ')
+                .replace(']', ' ')
+                .replace(", ", " ")
+                .trim());
             var userAnswer = Integer.parseInt(Engine.setUserAnswer());
             if (userAnswer == Integer.parseInt(correctAnswer)) {
                 Engine.correct();
