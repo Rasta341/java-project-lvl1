e5636c45196fd9a849e5250475aa698954a7758d
Rasta341 <Sir.pokerstar@yandex.ru>
2022-07-28 21:44:52 +0300

    make changes in Prime.java

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -1,24 +1,22 @@
 package hexlet.code.games;
+
 import hexlet.code.Engine;
+
 public class Prime {
     public static void prime() {
         Engine.greet();
         System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
-        final int[] primeNumbers = new int[] {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53};
         while (Engine.isWin()) {
-            int num = Engine.getRandomNum100();
+            int num = Engine.getRandomNum10();
             System.out.println("Question: " + num);
-            String correctAnswer = "";
+            String correctAnswer;
             String userAnswer = Engine.setUserAnswer();
-            for (int number : primeNumbers) {
-                if (number == num) {
-                    correctAnswer = "yes";
-                    break;
-                } else {
-                    correctAnswer = "no";
-                }
-                Engine.setCorrectAnswer(correctAnswer);
+            if (num % num == 0 || num % 1 == 0) {
+                correctAnswer = "yes";
+            } else {
+                correctAnswer = "no";
             }
+            Engine.setCorrectAnswer(correctAnswer);
             if (correctAnswer.equalsIgnoreCase(userAnswer)) {
                 Engine.correct();
             } else {
